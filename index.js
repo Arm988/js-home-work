24c3e5b27da85527a6bd81411774be238c2f1adb
Arman Petrosyan <117941537+Arm988@users.noreply.github.com>
2023-04-12 22:58:30 +0400

    add while js

---
+++ index.js
@@ -0,0 +1,94 @@
+"use strict"
+// let num = 3
+// let i = 0
+// while (i < num) {
+//   alert(i)
+//   i++
+// }
+
+// do {
+//   alert('value>>>>> ' + i )
+// } while(i > num)
+
+// for(; ;)
+
+// for (let i = 0; i < num ; i++){
+//   alert(i)
+// }
+
+// let g = +prompt('greq tiv ')
+// let i = 0
+// while( i <= g){
+//   alert(i)
+//   i++
+// }
+
+// let numER = +prompt('enter number')
+// let i = 0
+// if(numER){
+//   while(i < numER){
+//     console.log(i);
+//     i++
+//   }
+// }
+
+// let num = 3
+// let sum = 0
+// let i = 0
+// while (true) {
+//   let value = +prompt(' enter number')
+//   if(!value) break;
+//   sum += value;
+// }
+// alert(sum)
+
+// let a = num > 10 ? alert('kell') : alert('bolt')
+//let num = 10;
+//for(let i = 0; i < num; i++ ) {
+//  if(i % 2 == 0) continue;   //break; i depqum 
+//  alert(i);
+//}
+
+//alert('break erav') // breaki depqum 
+
+// let num = +prompt('enter number')
+// let resault = 0
+// if (num <= 10) {
+//   for (let i = 0 ; i <= num ; i++) {
+//   resault += i
+// } 
+// }
+// alert(resault)
+
+
+// let ourNumber = 7 
+// let userNumber = +prompt('number range 1-10')
+
+// while( ourNumber !== userNumber){
+//   if(userNumber > ourNumber)
+// } else {
+
+// }
+// userNumber = prompt
+
+let ournumber = 7
+
+
+// while(true){
+//   let usernumber = +prompt('number range 1-10')
+//   if(ournumber == usernumber){
+//   alert('yow win') 
+//   break;
+//   } else if(ournumber > usernumber){
+//   alert('your number smoll')
+//   continue;
+// } else { 
+//   alert('your number is big')
+// }
+// } 
+
+
+
+
+
+
